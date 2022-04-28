c6e52d5bdefc0b2479d2a0a3579d3b632e8be931
Everton Santos <evertonds@live.com>
2022-04-28 10:44:58 -0300

    Adicionando método find

--- 4. Strings com Python/main.py
+++ 4. Strings com Python/main.py
@@ -1,8 +1,10 @@
 url = "bytebank.com/cambio?moedaOrigem=real"
 print(url)
 
-url_base = url[0:19]
-print(url_base)  # Vai imprimir “bytebank.com/cambio”
 
-url_parametros = url[20:36]
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+print(url_base)
+
+url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
